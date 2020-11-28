514547a8d5fa02b55272598b75af69107f463639
ajeetyadav-a <74338137+ajeetyadav-a@users.noreply.github.com>
2020-11-28 12:20:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,70 +1,33 @@
-var score,roundscore,activeplayer,gamming;
-init();
-document.querySelector(".btn-roll").addEventListener("click",function(){
-if(gamming)
-{
-var domdice=document.querySelector(".dice");
-domdice.style.display="block";
-var randomnumber=Math.random()*6+1;
-var numbrdice=parseInt(randomnumber);
-domdice.src="dice-six-faces-"+numbrdice+".png";
-if(numbrdice!==1)
-{roundscore+=numbrdice;
-document.getElementById("current-"+activeplayer).textContent=roundscore;
-}
-else
-{
-nextplayer();
-}
-}
-});
-//btn hold
-document.querySelector(".btn-hold").addEventListener("click",function(){
-if(gamming)
-{
-score[activeplayer]+=roundscore;
-document.getElementById("score-"+activeplayer).textContent=score[activeplayer];
-//winner
-if(score[activeplayer]>=20)
-{
-gamming=false;
-document.getElementById("name-"+activeplayer).textContent="winner!";
-document.querySelector(".player-"+activeplayer+"-panel").classList.add("winner");
-document.querySelector(".dice").style.display="none";
-document.querySelector(".player-"+activeplayer+"-panel").classList.remove("active");
+let theme=localStorage.getItem('theme')
+if(theme ==null){
+    setTheme('light')
 }
 else{
-nextplayer();
+    setTheme(theme)
 }
+
+let themeDots=document.getElementsByClassName('theme-dot')
+for(var i=0;themeDots.length>i;i++){
+    themeDots[i].addEventListener('click',function(){
+        let mode=this.dataset.mode;
+        console.log('option clicked',mode);
+        setTheme(mode)
+    })
 }
-});
-function nextplayer()
-{
-roundscore=0;
-document.getElementById("current-0").textContent=0;
-document.getElementById("current-1").textContent=0;
-activeplayer===0?activeplayer=1:activeplayer=0;
-document.querySelector(".player-0-panel").classList.toggle("active");
-document.querySelector(".player-1-panel").classList.toggle("active");
-document.querySelector(".dice").style.display="none";
+function setTheme(mode){
+    if(mode=='light'){
+        document.getElementById('theme-style').href='default.css'
+    }
+    if(mode=='blue'){
+        document.getElementById('theme-style').href='blue.css'
+    }
+    if(mode=='green'){
+        document.getElementById('theme-style').href='green.css'
+    }
+    if(mode=='purple'){
+        document.getElementById('theme-style').href='purple.css'
+    }
+    localStorage.setItem('theme',mode);
 }
-document.querySelector(".btn-new").addEventListener("click",init);
-function init()
-{
-score=[0,0];
-roundscore=0;
-activeplayer=0;
-gamming=true;
-document.querySelector(".dice").style.display="none";
-document.getElementById("score-0").textContent='0';
-document.getElementById("score-1").textContent='0';
-document.getElementById("current-0").textContent='0';
-document.getElementById("current-1").textContent='0';
-document.getElementById("name-0").textContent='player1';
-document.getElementById("name-1").textContent='player2';
-document.querySelector(".player-0-panel").classList.remove("winner");
-document.querySelector(".player-1-panel").classList.remove("winner");
-document.querySelector(".player-1-panel").classList.remove("active");
-document.querySelector(".player-0-panel").classList.remove("active");
-document.querySelector(".player-0-panel").classList.add("active");
-}
+
+
